18ef8471cf1d228428db276b79839cc73199488d
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-15 05:03:36 -0500

    docs: Add docstring to euclid_algorithm_gcf function

--- greatest_common_factor/__init__.py
+++ greatest_common_factor/__init__.py
@@ -1,4 +1,11 @@
 def euclid_algorithm_gcf(a, b):
+    """
+    Calculate greatest common factor (gcf) using Euclid algorithm
+
+    :param a: (Integer)
+    :param b: (Integer)
+    :return: Integer
+    """
     if a == 0:
         return b
     elif b == 0:
