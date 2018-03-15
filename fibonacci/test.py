9a3122e2acca6cdfabe54815f402753dcda080b7
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-15 03:07:08 -0500

    feat: Add test_loop_list method

--- fibonacci/test.py
+++ fibonacci/test.py
@@ -11,4 +11,11 @@ class TestFibonacci(TestCase):
         self.assertEqual(get_fibonacci_recursion(6), 8)
         self.assertEqual(get_fibonacci_recursion(10), 55)
 
+    def test_loop_list(self):
+        self.assertEqual(get_fibonacci_loop_list(0), 0)
+        self.assertEqual(get_fibonacci_loop_list(1), 1)
+        self.assertEqual(get_fibonacci_loop_list(2), 1)
+        self.assertEqual(get_fibonacci_loop_list(6), 8)
+        self.assertEqual(get_fibonacci_loop_list(10), 55)
+
 
