ea65e797089b606f2898803ff5cd23b05cf19102
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-15 03:11:38 -0500

    feat: Add get_fibonacci_loop_list function

--- fibonacci/__init__.py
+++ fibonacci/__init__.py
@@ -4,3 +4,16 @@ def get_fibonacci_recursion(n):
     else:
         return get_fibonacci_recursion(n - 1) + get_fibonacci_recursion(n - 2)
 
+
+def get_fibonacci_loop_list(n):
+    fib_list = list()
+    if n < 2:
+        return n
+    else:
+        fib_list.append(0)
+        fib_list.append(1)
+        i = 2
+        while i < n + 1:
+            fib_list.append(fib_list[i - 1] + fib_list[i - 2])
+            i += 1
+    return fib_list[-1]
