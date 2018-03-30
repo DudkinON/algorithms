418a07066d9db600dedcb8503276f9c8e380c3a0
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-30 03:39:09 -0500

    feat: Add huffman codes package

---
+++ greedy_algorithms/huffman_codes/__init__.py
@@ -0,0 +1,50 @@
+import heapq
+from collections import Counter, namedtuple
+
+
+class Node(namedtuple("Node", ["left", "right"])):
+    def walk(self, code, acc):
+        self.left.walk(code, acc + "0")
+        self.right.walk(code, acc + "1")
+
+
+class Leaf(namedtuple("Leaf", ["char"])):
+    def walk(self, code, acc):
+        code[self.char] = acc or "0"
+
+
+def huffman_encode(string):
+    heap = []
+    for char, count in Counter(string).items():
+        heap.append((count, len(heap), Leaf(char)))
+
+    heapq.heapify(heap)
+
+    counter = len(heap)
+    while len(heap) > 1:
+        count_one, _count_one, left = heapq.heappop(heap)
+        count_two, _count_two, right = heapq.heappop(heap)
+        heapq.heappush(heap, (count_one + count_two, counter, Node(left, right)))
+        counter += 1
+
+    code = {}
+    if heap:
+        [(fq, _count, root)] = heap
+        root.walk(code, "")
+
+    return code
+
+
+def main():
+    string = raw_input()
+    code = huffman_encode(string)
+    encoded = "".join(code[character] for character in string)
+    print(len(code), len(encoded))
+    for char in sorted(code):
+        print("%s: %s" % (char, code[char]))
+
+    print(encoded)
+
+
+if __name__ == '__main__':
+    main()
