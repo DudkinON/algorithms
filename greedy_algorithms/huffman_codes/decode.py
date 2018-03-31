913c58b3b70c9cb1a734d9e5caaf7e3d52112404
Oleg Dudkin <dudkin.on@gmail.com>
2018-03-31 00:30:25 -0500

    docs: Add comments to Huffman decode

--- greedy_algorithms/huffman_codes/decode.py
+++ greedy_algorithms/huffman_codes/decode.py
@@ -1,34 +1,44 @@
 def huffman_decode(pairs, code):
+    # create variables
     result = ''
     temp = ''
+
+    # decode encoded string
     for char in code:
-        if len(temp):
-            temp += char
-        else:
-            temp = char
+        temp += char
 
         for key in pairs:
             if pairs[key] == temp:
                 result += key
                 temp = ''
+
     return result
 
 
 def main():
+
     pairs = {}
+
+    # read amount of pairs and length of encoded string
     amount, length = raw_input().split()
+
+    # convert to integer
     amount = int(amount)
     length = int(length)
 
+    # read pairs for decode and add to dictionary 'pairs'
     while amount > 0:
         key, value = raw_input().split(": ")
         pairs[key] = value
         amount -= 1
 
+    # read encoded string
     code = raw_input()
 
+    # check length of encoded string
     assert len(code) == length
 
+    # print result
     print(huffman_decode(pairs, code))
 
 
