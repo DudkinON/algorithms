be29f66eab3e3f953c0bc73e412d3294fa4af852
DudkinON <dudkin.on@gmail.com>
2019-01-10 20:50:03 -0600

    feat: Add class BinaryTreeNode

--- data_structures/__init__.py
+++ data_structures/__init__.py
@@ -3,3 +3,11 @@ class Node(object):
     def __init__(self, value):
         self.value = value
         self.next = None
+
+
+class BinaryTreeNode(object):
+
+    def __init__(self, value):
+        self.left = None
+        self.right = None
+        self.value = value
