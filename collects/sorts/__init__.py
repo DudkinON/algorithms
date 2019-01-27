1a2bd2eee6f603a02d5ce4bc596b1968b6b7b238
DudkinON <dudkin.on@gmail.com>
2019-01-27 11:06:03 -0800

    feat: Add selection sort algorithm

--- collects/sorts/__init__.py
+++ collects/sorts/__init__.py
@@ -17,11 +17,31 @@ def bubble_sort(lst):
     return lst
 
 
+def selection_sort(lst):
+
+    length = len(lst)
+
+    if length < 2:
+        return lst
+
+    for i in range(length):
+        smallest = i
+        temp = lst[i]
+        for j in range(i + 1, length):
+            if lst[smallest] > lst[j]:
+                smallest = j
+        lst[i] = lst[smallest]
+        lst[smallest] = temp
+
+    return lst
+
+
 def main():
     print("Enter numbers separated with whitespace: ")
     unsorted = [int(n) for n in stdin.readline().split()]
     print("unsorted list:\t", unsorted)
     print("bubble_sort:\t", bubble_sort(unsorted))
+    print("selection_sort:\t", selection_sort(unsorted))
 
 
 if __name__ == '__main__':
