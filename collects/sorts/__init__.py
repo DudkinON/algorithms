7395e9a729402e737349433abe3848b8d3d13171
DudkinON <dudkin.on@gmail.com>
2019-01-27 11:07:13 -0800

    feat: Add insertion sort angorithm

--- collects/sorts/__init__.py
+++ collects/sorts/__init__.py
@@ -36,12 +36,31 @@ def selection_sort(lst):
     return lst
 
 
+def insertion_sort(lst):
+
+    length = len(lst)
+
+    if length < 2:
+        return lst
+
+    for i in range(length):
+        if lst[i] < lst[0]:
+            lst = [lst[i]] + lst[:i] + lst[i + 1:]
+        else:
+            for j in range(1, i):
+                if lst[j - 1] <= lst[i] < lst[j]:
+                    lst = lst[:j] + [lst[i]] + lst[j:i] + lst[i + 1:]
+
+    return lst
+
+
 def main():
     print("Enter numbers separated with whitespace: ")
     unsorted = [int(n) for n in stdin.readline().split()]
     print("unsorted list:\t", unsorted)
     print("bubble_sort:\t", bubble_sort(unsorted))
     print("selection_sort:\t", selection_sort(unsorted))
+    print("insertion_sort:\t", insertion_sort(unsorted))
 
 
 if __name__ == '__main__':
