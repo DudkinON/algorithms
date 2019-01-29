58123ba1568716c50ea3dad79dc9ebd1a2bbacf9
DudkinON <dudkin.on@gmail.com>
2019-01-28 19:03:38 -0800

    feat: Add invert_string function

--- collects/recursion/__init__.py
+++ collects/recursion/__init__.py
@@ -4,3 +4,11 @@ def factorial(number):
 
 def fibonacci(n):
     return n if n < 2 else fibonacci(n-2) + fibonacci(n-1)
+
+
+def invert_string(s, result=""):
+    if len(s) > 0:
+        result += s[-1]
+        return invert_string(s[:-1], result)
+    else:
+        return result
