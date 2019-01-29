ecd29d83c88cd09460ef6291cdd1531a18d9ebc9
DudkinON <dudkin.on@gmail.com>
2019-01-28 19:31:00 -0800

    feat: Add cache function

--- collects/recursion/__init__.py
+++ collects/recursion/__init__.py
@@ -8,6 +8,18 @@ def factorial(number):
     return number if number < 3 else number * factorial(number - 1)
 
 
+def cache(f):
+
+    cache_data = {}
+
+    def inner(n):
+        if n not in cache_data:
+            cache_data[n] = f(n)
+        return cache_data[n]
+    return inner
+
+
+@cache
 def fibonacci(n):
     return n if n < 2 else fibonacci(n-2) + fibonacci(n-1)
 
