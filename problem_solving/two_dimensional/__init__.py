9b7d9c27ed398266de1956fecdd6cd9b773dbf60
Oleg Dudkin <dudkin.on@gmail.com>
2019-05-06 13:07:21 -0700

    docs: Add docs to rotate function

--- problem_solving/two_dimensional/__init__.py
+++ problem_solving/two_dimensional/__init__.py
@@ -2,9 +2,20 @@ from copy import deepcopy
 
 
 def rotate(arr, n):
+    """
+    Rotate two dimensional array by 90 degrees with additional
+    memory space complexity O(n) where n amaunt or elements in
+    array
 
+    :param arr: List[List[int]] - two dimensional array
+    :param n: Integer - size of array
+    :return: List[List[int]] - two dimensional array
+    """
+
+    # Create a copy of the array
     rotated = deepcopy(arr)
 
+    # Put to the new array new numbers
     for i in range(n):
         for j in range(n):
             (new_i, new_j) = (j, n - 1 - i)
