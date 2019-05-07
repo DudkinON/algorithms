24af5a0ca5c293c47d44a491a14350b66e7e3fe4
Oleg Dudkin <dudkin.on@gmail.com>
2019-05-07 16:52:05 -0700

    feat: Add rotate_less_memory function

--- problem_solving/two_dimensional/__init__.py
+++ problem_solving/two_dimensional/__init__.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+from math import ceil, floor
 
 
 def rotate(arr, n):
@@ -22,3 +23,21 @@ def rotate(arr, n):
             rotated[new_i][new_j] = arr[i][j]
 
     return rotated
+
+
+def rotate_less_memory(arr, n):
+
+    for i in range(ceil(n/2)):
+        for j in range(floor(n/2)):
+
+            temp, new_i, new_j = [], i, j
+
+            for k in range(4):
+                temp.append(arr[new_i][new_j])
+                new_i, new_j = new_j, n - 1 - new_i
+
+            for k in range(4):
+                arr[new_i][new_j] = temp[(k - 1) % 4]
+                new_i, new_j = new_j, n - 1 - new_i
+
+    return arr
