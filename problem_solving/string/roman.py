8de46387ae2ca95997b64fee78c050871ab867da
Oleg Dudkin <dudkin.on@gmail.com>
2019-05-18 10:44:34 -0700

    feat: Add roman_to_int function

---
+++ problem_solving/string/roman.py
@@ -0,0 +1,12 @@
+def roman_to_int(s: str) -> int:
+
+    cache = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+    total = cache[s[-1]]
+    for i in range(len(s) - 2, -1, -1):
+        value = cache[s[i]]
+        if cache[s[i + 1]] > value:
+            total -= value
+        else:
+            total += value
+    return total
