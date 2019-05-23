9fae9ae3c7055a502ff887cfc53b0dae8056d28b
Oleg Dudkin <dudkin.on@gmail.com>
2019-05-22 22:55:00 -0700

    feat: Add tests for merge function

--- problem_solving/array/tests.py
+++ problem_solving/array/tests.py
@@ -1,5 +1,6 @@
 from unittest import TestCase
 from problem_solving.array.three_sum import three_sum
+from problem_solving.array.merge import merge
 
 
 class TestArrays(TestCase):
@@ -13,3 +14,18 @@ class TestArrays(TestCase):
         self.assertEqual(three_sum(arr2), [])
         self.assertEqual(three_sum(arr3), [])
         self.assertEqual(three_sum(arr4), [[0, 0, 0]])
+
+    def test_merge(self):
+        case1 = [1, 2, 3, 0, 0, 0]
+        case2 = [0, 0, 0, 0, 0]
+        case3 = [4, 0, 0, 0, 0, 0]
+        case4 = [1, 2, 4, 5, 6, 0]
+        merge(case1, 3, [2, 5, 6], 3)
+        merge(case2, 1, [1, 2, 4, 6], 4)
+        merge(case3, 1, [1, 2, 3, 5, 6], 5)
+        merge(case4, 5, [3], 1)
+
+        self.assertEqual(case1, [1, 2, 2, 3, 5, 6])
+        self.assertEqual(case2, [0, 1, 2, 4, 6])
+        self.assertEqual(case3, [1, 2, 3, 4, 5, 6])
+        self.assertEqual(case4, [1, 2, 3, 4, 5, 6])
