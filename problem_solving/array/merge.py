09922cb756f85138d34b94a2507f45166a271052
Oleg Dudkin <dudkin.on@gmail.com>
2019-05-22 22:53:20 -0700

    docs: Add docs to merge function

--- problem_solving/array/merge.py
+++ problem_solving/array/merge.py
@@ -2,7 +2,17 @@ from typing import List
 
 
 def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+    """
+    Merges two sorted arrays in place, doesn't returns anything. After
+    the function is done, the first array will be sorted and will
+    contain all elements from the second array
 
+    :param nums1: List[int] - First array
+    :param m: Integer - length of first array
+    :param nums2: List[int] - Second array
+    :param n: Integer - length of second array
+    :return: None
+    """
     i = len(nums1) - 1
     last1 = m - 1
     last2 = n - 1
