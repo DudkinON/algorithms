9b56469e26a5d94a2d6094967e7f444b2705a737
Oleg Dudkin <dudkin.on@gmail.com>
2019-05-24 14:31:37 -0700

    feat: Add group_anagrams function

---
+++ problem_solving/array/anagram.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+def group_anagrams(strs: List[str]) -> List[List[str]]:
+
+    _map = {}
+
+    for item in strs:
+
+        _hash = ''.join(sorted(item))
+
+        if _map.get(_hash) is None:
+            _map[_hash] = []
+        _map[_hash].append(item)
+
+    return list(_map.values())
