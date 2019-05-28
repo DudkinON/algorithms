629b54d0b7719574c2dcef7cca922c25fdbc1ee5
Oleg Dudkin <dudkin.on@gmail.com>
2019-05-27 19:50:42 -0700

    docs: Add doc to add_binary function

--- problem_solving/lib/binary.py
+++ problem_solving/lib/binary.py
@@ -1,2 +1,9 @@
 def add_binary(a, b):
+    """
+    Add two binary strings and return result
+    
+    :param a: String
+    :param b: String
+    :return: String
+    """
     return bin(int(a, 2) + int(b, 2))[2:]
