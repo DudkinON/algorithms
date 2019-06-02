91f8deef0ea026ca9f9579855f65e42cb5412b0a
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-02 10:35:31 -0700

    docs: Add doc to is_anagram function

--- problem_solving/string/anagram.py
+++ problem_solving/string/anagram.py
@@ -2,6 +2,13 @@ from collections import Counter
 
 
 def is_anagram(s: str, t: str) -> bool:
+    """
+    Checks that first string is anagram of second, return boolean
+
+    :param s: String
+    :param t: String
+    :return: Boolean
+    """
 
     if len(s) != len(t):
         return False
