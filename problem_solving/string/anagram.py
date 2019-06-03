7bc148904429e42d0d8781024d440145df973a5b
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-03 11:37:13 -0700

    feat: Add pal_anagram function

--- problem_solving/string/anagram.py
+++ problem_solving/string/anagram.py
@@ -22,3 +22,32 @@ def is_anagram(s: str, t: str) -> bool:
         counter[char] -= 1
 
     return True
+
+
+def pal_anagram(s: str) -> bool:
+
+    _map = [0 for _ in range(ord('z') - ord('a') + 1)]
+
+    a, z, A, Z = ord('a'), ord('z'), ord('A'), ord('Z')
+
+    odds = 0
+
+    for char in s:
+        val = ord(char)
+
+        if a <= val <= z:
+            index = val - a
+        elif A <= val <= Z:
+            index = val - A
+        else:
+            index = -1
+
+        if index > -1:
+            _map[index] += 1
+
+            if _map[index] % 2 == 1:
+                odds += 1
+            else:
+                odds -= 1
+
+    return odds < 2
