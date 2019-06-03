ed7f889511a40af286470cc25d0e7f37e6541b2c
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-03 13:18:40 -0700

    feat: Add on_away function

---
+++ problem_solving/string/one_away.py
@@ -0,0 +1,33 @@
+def one_away(s: str, t: str) -> bool:
+
+    len_diff = abs(len(s) - len(t))
+
+    if len_diff > 1:
+        return False
+
+    elif len_diff == 0:
+        diff = 0
+        for i in range(len(s)):
+            if s[i] != t[i]:
+                diff += 1
+                if diff > 1:
+                    return False
+        return True
+
+    else:
+        max_s = s if len(s) > len(t) else t
+        min_s = t if len(s) > len(t) else s
+
+        diff = i = j = 0
+
+        while i < len(min_s) and j < len(max_s):
+            if min_s[i] != max_s[j]:
+                diff += 1
+                j += 1
+                if diff > 1:
+                    return False
+            else:
+                j += 1
+                i += 1
+
+        return True
