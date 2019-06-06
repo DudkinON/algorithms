ee88370bd290e5df09ccb20635de2b4e0dc51c1a
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-06 13:04:36 -0700

    feat: Add tests for rotate_matrix function

--- problem_solving/two_dimensional/tests.py
+++ problem_solving/two_dimensional/tests.py
@@ -1,6 +1,7 @@
 from unittest import TestCase
 from problem_solving.two_dimensional import rotate, rotate_less_memory
 from problem_solving.two_dimensional import click, mine_sweeper, bfs
+from problem_solving.two_dimensional import rotate_matrix
 
 
 class TwoDimensionalTests(TestCase):
@@ -103,7 +104,6 @@ class TwoDimensionalTests(TestCase):
         self.assertEqual(result3, a3)
 
     def test_bfs(self):
-
         grid = [
             [0, 0, 0, 0, 9, 0, 0],
             [0, 9, 9, 0, 0, 0, 0],
@@ -117,3 +117,34 @@ class TwoDimensionalTests(TestCase):
 
         self.assertEqual(bfs(grid, (0, 0)), a1)
         self.assertEqual(bfs(grid, (5, 3)), a2)
+
+    def test_rotate_matrix(self):
+        data = [
+            (
+                [
+                    [1, 2, 3],
+                    [4, 5, 6],
+                    [7, 8, 9]
+                ], [
+                    [7, 4, 1],
+                    [8, 5, 2],
+                    [9, 6, 3]
+                ]
+            ),
+            ([
+                 [1,  2,  3,  4,  5],
+                 [6,  7,  8,  9,  10],
+                 [11, 12, 13, 14, 15],
+                 [16, 17, 18, 19, 20],
+                 [21, 22, 23, 24, 25]
+             ], [
+                 [21, 16, 11, 6,  1],
+                 [22, 17, 12, 7,  2],
+                 [23, 18, 13, 8,  3],
+                 [24, 19, 14, 9,  4],
+                 [25, 20, 15, 10, 5]
+             ])
+        ]
+        for [test_matrix, expected] in data:
+            actual = rotate_matrix(test_matrix)
+            self.assertEqual(actual, expected)
