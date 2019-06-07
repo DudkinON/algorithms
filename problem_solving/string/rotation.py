25df926e48ab620d7d78802272ae486bf429913a
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-07 14:57:59 -0700

    docs: Add docs to string_rotation function

--- problem_solving/string/rotation.py
+++ problem_solving/string/rotation.py
@@ -1,3 +1,9 @@
 def string_rotation(s: str, t: str) -> bool:
+    """
+    Checks that string 't' is rotation of string 's'
 
+    :param s: String
+    :param t: String
+    :return: Boolean
+    """
     return (s + s).find(t) != -1 if len(s) == len(t) > 0 else False
