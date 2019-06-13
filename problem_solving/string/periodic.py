0f0a0fc96f1776b52bd970ff7b75d85a3b52745d
Oleg Dudkin <dudkin.on@gmail.com>
2019-06-13 13:26:42 -0700

    feat: Add periodic function

---
+++ problem_solving/string/periodic.py
@@ -0,0 +1,25 @@
+def periodic(s: str) -> str:
+
+    if len(s) == 0:
+        return ""
+
+    period = s[0]
+
+    for i in range(1, len(s)):
+
+        period_length = len(period)
+        start = len(s) - period_length
+        end = start + period_length % len(s)
+
+        if period == s[start:end]:
+
+            while period == s[start:end] and start > 0:
+                start -= period_length
+                end = start + period_length
+
+            if start == 0:
+                return period
+
+        period += s[i]
+
+    return period
